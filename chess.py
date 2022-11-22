6e7b143221f613ebf1b12ce2c976366a59fcd3a3
M. Balfroid <martin.balfroid@unamur.be>
2022-11-22 14:33:57 +0100

    Generate base code with copilot

--- chess.py
+++ chess.py
@@ -37,131 +37,45 @@
 
 class BoardState:
     def __init__(self):
-        self.pawns = [1] * 16
-        self.rooks = [2] * 4
-        self.knights = [3] * 4
-        self.bishops = [4] * 4
-        self.king = [5] * 2
-        self.queen = [6] * 2
+        self.pieces = {}
+        self.pieces["Pawns"] = 8
+        self.pieces["Rooks"] = 2
 
-    def number_of_pieces(self, piece: str):
-        if piece == "Pawns":
-            return len(self.pawns)
-        elif piece == "Rooks":
-            return len(self.rooks)
-        elif piece == "Knights":
-            return len(self.knights)
-        elif piece == "Bishops":
-            return len(self.bishops)
-        elif piece == "Kings":
-            return len(self.king)
-        elif piece == "Queens":
-            return len(self.queen)
+    def number_of_pieces(self, piece):
+        return self.pieces[piece]
 
     def set_piece(self, piece, position):
         pass
-    
+
     def move_piece(self, piece, position):
         pass
 
     def get_piece(self, position):
         pass
 
-class PiecesSet:
-
+class Rules:
     def __init__(self):
         pass
 
-    def full_set(self, type="All", color="All"):
-        if type is "Pawns" and color is "All":
-            return [1] * 16
-        if color is "White" and type is "Pawns":
-            return [1] * 8
-        else:
-            return [1] * 32
+class PiecesSet:
+    def __init__(self):
+        pass
 
     def setup_pieces(self):
         pass
 
-    def starting_positions(self, piece):
-        pass
-
     def starting_locations(self, color, piece):
         pass
-        
-
-class Rules:
-
-    def __init__(self, game="Chess"):
-        self.game = game
-        self.list_of_pieces = ([
-            "Pawns",
-            "Rooks",
-            "Knights",
-            "Bisops",
-            "Kings",
-            "Queen",
-        ])
-
-    def number_of_pieces(self, *piece):
-        if self.game == "Chess":
-            pieces_per_player = ({
-            "Pawns": 8,
-            "Rooks": 2,
-            "Knights": 2,
-            "Bisops": 2,
-            "Kings": 1,
-            "Queen": 1,
-            })
-            return pieces_per_player[piece]
-        else:
-            return "This game is not supported"
-
-    def starting_locations(self, **parameters):
-        if self.game == "Chess":
-            if parameters["color"] == "White":
-                pieces_locations = ({
-                "Pawns": ["a2","b2","c2","g2","e2","f2","g2","h2"],
-                "Rooks": ["a1","h1"],
-                "Knights": ["b1","g1"],
-                "Bisops": ["c1","f1"],
-                "Kings": ["d1"],
-                "Queen": ["e1"],
-                })
-                return pieces_per_player[parameters["piece"]]
-            if parameters["color"] == "Black":
-                pieces_locations = ({
-                "Pawns": ["a7","b7","c7","g7","e7","f7","g7","h7"],
-                "Rooks": ["a8","h8"],
-                "Knights": ["b8","g8"],
-                "Bisops": ["c8","f8"],
-                "Kings": ["d8"],
-                "Queen": ["e8"],
-                })
-                return pieces_per_player[parameters["piece"]]
-            else:
-                return "Incorrect color or piece code"
-        else:
-            return "This game is not supported"
 
 class Chess:
-
-
-
     def __init__(self):
+        self.board = []
         self.board_state = BoardState()
         self.rules = Rules()
-        self.score = 0
-        self.default_state = 0
-        self.num_rows = 8
-        self.game_active = False
-        self.board = [ [self.default_state] * self.num_rows] * self.num_rows
+        self.pieces_set = PiecesSet()
 
     def start_new_game(self):
-        self.game_active = True
-
-    def piece(self, column, row):
-        return {"color": "Black", "type": "Pawn"}
+        self.board = [[0 for x in range(8)] for y in range(8)]
 
     
     
